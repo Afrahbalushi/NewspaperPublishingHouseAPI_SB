9cc952ea0373e8a274fe908ed11d27371aef0b0f
Afrah Ali <116621717+Afrahbalushi@users.noreply.github.com>
2023-06-11 14:10:16 +0400

    Add Post method

--- NewspaperPublishingHouseAPI/src/main/java/com/example/NewspaperPublishingHouseAPI/Controllers/ArticlesController.java
+++ NewspaperPublishingHouseAPI/src/main/java/com/example/NewspaperPublishingHouseAPI/Controllers/ArticlesController.java
@@ -1,5 +1,45 @@
 package com.example.NewspaperPublishingHouseAPI.Controllers;
 
+import com.example.NewspaperPublishingHouseAPI.Models.Articles;
+import com.example.NewspaperPublishingHouseAPI.Services.ArticlesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/articles")
 public class ArticlesController {
+
+    @Autowired
+    ArticlesService articlesService;
+
+    @PostMapping(value = "/createRecord")
+    public String articlesRecord(@RequestParam String title, String content, String author, Date publishDate) {
+        try{
+            articlesService.articlesRecord(title,content,author,publishDate);
+        }catch (Exception e){
+            return "Fail to create!";
+        }
+        return "Created!";
+    }
+
+        @GetMapping(value = "/getAll")
+    public List<Articles> getAllArticles(){
+        List<Articles> articles = new ArrayList<>();
+        articles = articlesService.getAllArticles();
+        return articles;
+    }
+
+    @GetMapping(value = "/getById")
+    public Articles getArticleById(@RequestParam Integer articleId){
+        Articles articles = articlesService.getArticleById(articleId);
+        return articles;
+    }
+
+
+
 
 }
--- NewspaperPublishingHouseAPI/src/main/java/com/example/NewspaperPublishingHouseAPI/Services/ArticlesService.java
+++ NewspaperPublishingHouseAPI/src/main/java/com/example/NewspaperPublishingHouseAPI/Services/ArticlesService.java
@@ -5,12 +5,24 @@ import com.example.NewspaperPublishingHouseAPI.Repositories.ArticlesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class ArticlesService {
     @Autowired
     ArticlesRepository articlesRepository;
+
+
+    public void articlesRecord(String title, String content, String author, Date publishDate){
+        Articles article = new Articles();
+        article.setTitle(title);
+        article.setContent(content);
+        article.setAuthor(author);
+        article.setPublishDate(new Date());
+        articlesRepository.save(article);
+    }
+
 
     public Articles getArticleById(Integer id){
         Articles articles = articlesRepository.getArticlesById(id);
