d24b0f699e9b2867cd74a6a5083cda5cdca8de99
Afrah Ali <116621717+Afrahbalushi@users.noreply.github.com>
2023-06-11 11:42:48 +0400

    Add attributes to Articles class

--- NewspaperPublishingHouseAPI/src/main/java/com/example/NewspaperPublishingHouseAPI/Models/Articles.java
+++ NewspaperPublishingHouseAPI/src/main/java/com/example/NewspaperPublishingHouseAPI/Models/Articles.java
@@ -1,4 +1,25 @@
 package com.example.NewspaperPublishingHouseAPI.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Getter
+@Setter
+@Data
 public class Articles {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)        
+    Integer id;
+    String title;
+    String content;
+    String author;
+    Date publishDate;
+
 }
