ce104e88ea844c11f8e1036c5751bfe7b4c7811c
Afrah Ali <116621717+Afrahbalushi@users.noreply.github.com>
2023-06-11 12:02:29 +0400

    Article interface class creation

---
+++ NewspaperPublishingHouseAPI/src/main/java/com/example/NewspaperPublishingHouseAPI/Repositories/ArticlesRepository.java
@@ -0,0 +1,10 @@
+package com.example.NewspaperPublishingHouseAPI.Repositories;
+
+import com.example.NewspaperPublishingHouseAPI.Models.Articles;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticlesRepository extends JpaRepository<Articles,Integer> {
+    
+}
