c33c7bc63b7a54b1851ad383f15437a50592bd6c
Afrah Ali <116621717+Afrahbalushi@users.noreply.github.com>
2023-06-12 10:59:20 +0400

    updated

--- NewspaperPublishingHouseAPI/src/main/java/com/example/NewspaperPublishingHouseAPI/Controllers/ReportController.java
+++ NewspaperPublishingHouseAPI/src/main/java/com/example/NewspaperPublishingHouseAPI/Controllers/ReportController.java
@@ -1,6 +1,5 @@
 package com.example.NewspaperPublishingHouseAPI.Controllers;
 
-import com.example.NewspaperPublishingHouseAPI.Services.ArticlesService;
 import com.example.NewspaperPublishingHouseAPI.Services.ReportService;
 import net.sf.jasperreports.engine.JRException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.FileNotFoundException;
 
+
 @RestController
-@RequestMapping(value = "report")
+@RequestMapping(value = "/report")
 public class ReportController {
     @Autowired
     ReportService reportService;
@@ -20,4 +20,6 @@ public class ReportController {
     public String generateReport() throws FileNotFoundException, JRException{
         return reportService.generateReport();
     }
+
+
 }
--- NewspaperPublishingHouseAPI/src/main/java/com/example/NewspaperPublishingHouseAPI/Services/ReportService.java
+++ NewspaperPublishingHouseAPI/src/main/java/com/example/NewspaperPublishingHouseAPI/Services/ReportService.java
@@ -35,4 +35,5 @@ public class ReportService {
         return "Report Generated!!";
     }
 
+
 }
